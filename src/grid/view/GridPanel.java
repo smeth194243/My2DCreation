4165e0035901214e2ab626389918628bd385d5e7
smeth194243 <seth.r.m1@gmail.com>
2017-01-30 13:34:55 -0700

    Did some panel

--- src/grid/view/GridPanel.java
+++ src/grid/view/GridPanel.java
@@ -1,9 +1,16 @@
 package grid.view;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 import grid.view.GridFrame;
+import java.awt.Color;
+import grid.controller.GridController;
+import java.awt.Dimension;
 
 public class GridPanel extends JPanel
 {
+	private GridController baseController;
+	private SpringLayout baseLayout;
+	private JButton updateButton;
+	
 	
 }
