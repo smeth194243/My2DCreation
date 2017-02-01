b42bbf84be5e1ed4e83f201017d25505f0b231fe
smeth194243 <seth.r.m1@gmail.com>
2017-02-01 13:23:13 -0700

    shtarted on shum shetupTable

--- src/grid/view/GridPanel.java
+++ src/grid/view/GridPanel.java
@@ -16,7 +16,23 @@ public class GridPanel extends JPanel
 	private JTextField somethingElseField;
 	private JTable tableThing;
 	
-	public GridPanel
+	public GridPanel(GridController baseController)
+	{
+		this.baseController = baseController;
+		this.baseLayout = baseLayout;
+		this.columnField = new JTextField(5);
+		
+		setupTable();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	public void setupTable()
+	{
+		DefaultTableModel data = new DefalutTableModel(baseController.getGrid(), new String []{"one","two","three","four","five"});
+	}
+	
 	
 }
 
