7f76106c48152d090c46271b977c08508f47397f
smeth194243 <seth.r.m1@gmail.com>
2017-02-01 12:44:30 -0700

    more panel on DD project

--- src/grid/view/GridPanel.java
+++ src/grid/view/GridPanel.java
@@ -11,6 +11,12 @@ public class GridPanel extends JPanel
 	private GridController baseController;
 	private SpringLayout baseLayout;
 	private JButton updateButton;
+	private JTextField columnField;
+	private JTextField rowField;
+	private JTextField somethingElseField;
+	private JTable tableThing;
 	
+	public GridPanel
 	
 }
+
